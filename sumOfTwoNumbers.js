06f746f20a1981fb2df9f937f0482382e4e4477f
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-04 14:00:23 +0530

    first commit

---
+++ sumOfTwoNumbers.js
@@ -0,0 +1,9 @@
+// Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+
+function myFunction(a, b) {
+  return a + b;
+}
+
+myFunction(1, 2); // Expected 3;
+myFunction(1, 10); // Expected 11;
+myFunction(99, 1); // Expected 100;
