ef2b5b7e2c08134d15be3834b41b54447740e924
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-09 10:03:50 +0530

    Count number of elements in JavaScript array

---
+++ JavaScript-Arrays/arrayLength.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array (a) as argument. Return the number of elements in a.
+function arrayLength(a) {
+  console.log(a.length);
+}
+
+arrayLength([1, 2, 2, 4]); //Expected 4
+arrayLength([9, 9, 9]); //Expected 3
+arrayLength([4, 3, 2, 1, 0]); //Expected 5
