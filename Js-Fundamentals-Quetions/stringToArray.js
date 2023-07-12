80462dc10d4488e035d39947f34c8eeb724c895a
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-12 14:49:18 +0530

    converting strings to array

---
+++ Js-Fundamentals-Quetions/stringToArray.js
@@ -0,0 +1,8 @@
+// converting the string to an array
+
+const stringToArray = (string) => {
+  const result = string.split(" ");
+  console.log(result);
+};
+stringToArray("hello"); // ["hello"]
+stringToArray("hello world"); // ["hello"]
