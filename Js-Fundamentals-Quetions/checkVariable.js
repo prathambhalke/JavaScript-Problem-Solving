d09fa316b9f692f9b07a3b197ad97293de03e552
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-17 22:53:07 +0530

    Check undefined or null

---
+++ Js-Fundamentals-Quetions/checkVariable.js
@@ -0,0 +1,9 @@
+// JavaScript Program To Check If A Variable Is undefined or null
+
+const checkVariable = (variable) => {
+  return variable;
+};
+
+console.log(checkVariable(5));
+console.log(checkVariable("hello"));
+console.log(checkVariable(null));
