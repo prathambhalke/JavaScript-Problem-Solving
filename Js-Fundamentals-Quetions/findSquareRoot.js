d8cfedc6dc3d5820c8507e0926942d6be809e795
prathambhalke <prathameshbhalke125@gmail.com>
2023-07-17 23:01:43 +0530

    Find the Square Root

---
+++ Js-Fundamentals-Quetions/findSquareRoot.js
@@ -0,0 +1,9 @@
+// JavaScript Program to Find the Square Root
+
+const findSquaRoot = (number) => {
+  return Math.sqrt(number);
+};
+
+console.log(findSquaRoot(2.25)); // Expected 1.5
+console.log(findSquaRoot(-4)); // Expected NaN
+console.log(findSquaRoot("hello")); //Expected NaN
